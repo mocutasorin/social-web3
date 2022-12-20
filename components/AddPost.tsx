5fdac51dcdf4f23934adb9c013ff50770f143db4
Mocuta Sorin <mocutasorin@gmail.com>
2022-12-20 23:54:10 +0200

    add items to left nav + add post section UI

--- components/AddPost.tsx
+++ components/AddPost.tsx
@@ -1,13 +1,42 @@
-const AddPost = () => (
-  <div className="bg-white border-gray-200 border-solid border p-3">
-    <h2>ADD POST</h2>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit nostrum
-      consectetur repellat praesentium voluptatibus voluptate unde perspiciatis?
-      Accusamus blanditiis maiores aperiam porro adipisci qui. Architecto cumque
-      mollitia rerum nisi dicta.
-    </p>
-  </div>
-);
+import { SyntheticEvent, useRef } from "react";
+import { BsImage } from "react-icons/bs";
+
+const AddPost = () => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleChange = (e: SyntheticEvent) => {
+    const target = e.target as HTMLTextAreaElement;
+    if (textareaRef.current === null) return;
+    textareaRef.current.style.height = "40px";
+    textareaRef.current.style.height = `${target.scrollHeight}px`;
+  };
+  return (
+    <div className="bg-white border-gray-200 border-solid border p-10">
+      {/* Text input */}
+      <div className="flex flex-1 flex-row w-full">
+        <img
+          className="h-8 w-8 rounded-full"
+          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt=""
+        />
+        <textarea
+          ref={textareaRef}
+          placeholder="What's in your mind, Sam Bankman?"
+          className="ml-5 w-full overflow-hidden focus:outline-none text-gray-600"
+          onChange={handleChange}
+        ></textarea>
+      </div>
+      {/* Call to action buttons */}
+      <div>
+        <div>
+          <a href="#" className="flex items-center text-gray-800 font-semibold">
+            <BsImage className="mr-3 text-xl" />
+            Photo/Video
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AddPost;
--- components/VerticalMenu.tsx
+++ components/VerticalMenu.tsx
@@ -1,8 +1,8 @@
-import { BsBroadcast, BsEnvelope } from "react-icons/bs";
+import { BsBroadcast, BsCart, BsEnvelope } from "react-icons/bs";
 
 export default function VerticalMenu() {
   return (
-    <div className=" bg-bg-verticalMenu pt-4 w-full sm:w-1/5 flex">
+    <div className=" bg-bg-verticalMenu pt-4 w-full sm:w-1/5 flex flex-grow-0">
       <ul className="pt-0 sm:pt-4 flex flex-1 flex-row sm:flex-col">
         <li>
           <a
@@ -20,6 +20,33 @@ export default function VerticalMenu() {
           >
             <BsEnvelope className="mr-4 text-2xl" />
             Messages
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="flex items-center pl-4 py-3 text-base font-semibold border-l-4 border-bg-verticalMenu text-cyan-700 transition ease-in-out delay-150 hover:bg-bg-hoverVertical hover:border-cyan-700 hover:border-l-4"
+          >
+            <BsCart className="mr-4 text-2xl" />
+            Marketplace
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="flex items-center pl-4 py-3 text-base font-semibold border-l-4 border-bg-verticalMenu text-cyan-700 transition ease-in-out delay-150 hover:bg-bg-hoverVertical hover:border-cyan-700 hover:border-l-4"
+          >
+            <BsCart className="mr-4 text-2xl" />
+            Events
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="flex items-center pl-4 py-3 text-base font-semibold border-l-4 border-bg-verticalMenu text-cyan-700 transition ease-in-out delay-150 hover:bg-bg-hoverVertical hover:border-cyan-700 hover:border-l-4"
+          >
+            <BsCart className="mr-4 text-2xl" />
+            Groups
           </a>
         </li>
       </ul>
